f4542b13f61ef752c7807e8ff02b5922a596ad41
VaiBhavDJain <hiivaibhav1@gmail.com>
2025-08-12 17:53:09 +0530

    After ran this command  rails turbo:install stimulus:install some files are auto generated

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,7 @@
 
 pin "application", preload: true  # This pins the application.js file
 pin "timer-apply", to: "timer.js"      # Pinning the timer.js file
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
